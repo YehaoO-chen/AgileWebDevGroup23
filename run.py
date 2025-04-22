fde4cfae8f0ac0ddae35d35e6acb6e8d22a9a2f2
YehaoO-chen <chenyehao00@qq.com>
2025-04-22 09:08:45 +0800

    update

--- run.py
+++ run.py
@@ -1,16 +1,6 @@
-<<<<<<< HEAD
 from app import create_app
 
 app = create_app()
 
 if __name__ == '__main__':
     app.run(debug=True)
-=======
-from flask import Flask
-from flask import render_template
-app = Flask(__name__)
-
-@app.route('/')
-def home(name=None):
-    return render_template('home.html')
->>>>>>> cf24331 (set a project)
