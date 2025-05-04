aef3cfed2be5b01a2b5d5936e9d5a6749e37ce9a
chloe5yy <117055665+Chloeiw@users.noreply.github.com>
2025-05-04 06:49:57 +0000

    update profile api

--- app/models.py
+++ app/models.py
@@ -1,14 +1,19 @@
 from flask_login import UserMixin
-from datetime import datetime
+from datetime import datetime, timezone
 from app.extensions import db, login_manager
 
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(150), unique=True, nullable=False)
+    username = db.Column(db.String(150), unique=True, nullable=False,index=True)
     password = db.Column(db.String(150), nullable=False)
     security_answer = db.Column(db.String(150), nullable=False)
-    create_time = db.Column(db.DateTime, default=datetime.utcnow)
-
+    create_time = db.Column(db.DateTime, default=datetime.now(timezone.utc))
+    fullname = db.Column(db.String(150), nullable=True)
+    major = db.Column(db.String(50), nullable=True)
+    email = db.Column(db.String(150), unique=True, nullable=True)
+    phone = db.Column(db.String(20), unique=True, nullable=True)
+    address = db.Column(db.String(200), nullable=True)
+    student_id = db.Column(db.String(20), unique=True, nullable=True)
     def __repr__(self):
         return f'<User {self.username}>'
 
@@ -24,7 +29,7 @@ class StudyPlan(db.Model):
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
     title = db.Column(db.String(200), nullable=False)
     content = db.Column(db.String(200), nullable=False)
-    create_time = db.Column(db.DateTime, default=datetime.utcnow, nullable=False)
+    create_time = db.Column(db.DateTime, default=datetime.now(timezone.utc), nullable=False)
     complete_time = db.Column(db.DateTime, nullable=True)
     status = db.Column(db.Integer, default=0, nullable=False)  # 0: open, 1: completed, 2: deleted
     
