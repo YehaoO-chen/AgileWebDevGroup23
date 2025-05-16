2f9622b22725effffef74cb946a5635182bfff39
chloe5yy <117055665+Chloeiw@users.noreply.github.com>
2025-05-16 12:35:44 +0000

    rm studyplan title

--- app/models.py
+++ app/models.py
@@ -63,7 +63,6 @@ class StudyPlan(db.Model):
     status = db.Column(db.Integer, default=0, nullable=False)  # 0: open, 1: completed, 2: deleted
     
     user = db.relationship('User', backref=db.backref('study_plans', lazy=True))
-    title = db.Column(db.String(100), nullable=False, default='-')
     def __repr__(self):
         return f'<StudyPlan {self.content}>'
 
